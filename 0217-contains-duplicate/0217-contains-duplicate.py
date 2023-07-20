dc548be82d0d0a0a66c12531a3d87c64683c2f3e
Madhukar R <95488077+Madhukar-R12@users.noreply.github.com>
2023-07-20 16:05:33 +0530

    Time: 563 ms (31.83%), Space: 33.6 MB (10.30%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -6,11 +6,8 @@ class Solution:
             if nums[i] not in d:
                 d[nums[i]] = 1
             else:
-                d[nums[i]] +=1
-        for k,v in d.items():
-            if v>1:
                 return True
-        return False    
+        return False
     
         # res = set(nums)
         # if len(nums)==len(res):
